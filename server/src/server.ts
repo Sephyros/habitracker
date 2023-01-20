772daca4f215c74a4595b8ae8cba1c706b8db14c
Sephyros <leo037br@hotmail.com>
2023-01-20 16:50:08 -0300

    Prisma and first model (habits) initialization

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
+import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
 
 const app = Fastify()
+const prisma = new PrismaClient
 
-app.get('/', () => {
-  return "Habitracker"
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany()
+  return habits
 })
 
 app.listen({
   port: 3333
-})
+}).then(
+  () => {
+    console.log(`Server Running on port 3333`)
+  }
+)
