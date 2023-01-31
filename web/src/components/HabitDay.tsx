46f0bef414738f755b894d1c292f25d39e61c9ae
Sephyros <leo037br@hotmail.com>
2023-01-30 23:24:23 -0300

    WEB: Created habit popover and progressbar

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,17 @@
+import * as Popover from '@radix-ui/react-popover'
+import { ProgressBar } from './ProgressBar'
 export function HabitDay() {
   return (
-    <div className="w-10 h-10 bg-zinc-800 border-2 border-zinc-700 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-800 border-2 border-zinc-700 rounded-lg" />
+      <Popover.Portal>
+        <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-800 flex flex-col border-[1px] border-violet-500'>
+          <span className='font-semibold text-zinc-400'>terça-feira</span>
+          <span className='mt-1 font-extrabold leading-tight tex-3xl'>17/01</span>
+          <ProgressBar progress={15} />
+          <Popover.Arrow height={8} width={16} className='fill-violet-500' />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
