20ca533737f1efe6c26cc1f1b592750f09fb12f2
Sephyros <leo037br@hotmail.com>
2023-02-02 23:35:16 -0300

    Fixed connection issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,31 @@ import Fastify from "fastify";
 import cors from "@fastify/cors"
 import { appRoutes } from "./lib/routes";
 
-const app = Fastify()
+const app = Fastify({ logger: true })
 
 app.register(cors, {
-  origin: '*'
+  origin: [
+    'http://192.168.0.149:19000', // Mobile
+    'http://localhost:5173' // Web
+  ]
 })
 app.register(appRoutes)
 app.listen({
-  port: 3333
-}).then(
-  () => {
-    console.log(`Server Running on port 3333`)
+  port: 3333,
+  host: '0.0.0.0'
+}, (err, address) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
   }
-)
+  app.log.info(`Server listening on ${address}`)
+})
+
+// app.listen({
+//   port: 3333,
+//   host: '0.0.0.0'
+// }).then(
+//   () => {
+//     console.log(`Server Running on port 3333`)
+//   }
+// )
