0a298c9ab5e7978f225c5e8e20a4ef7ec933bf0f
Sephyros <leo037br@hotmail.com>
2023-02-05 03:39:03 -0300

    MOBILE: Added some animations

--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,25 +1,38 @@
 import clsx from "clsx"
+import { useEffect } from "react"
 import { View } from "react-native"
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated"
 
 interface Props {
   progress?: number
 }
+
 export function ProgressBar({ progress = 0 }: Props) {
+  const sharedProgress = useSharedValue(progress)
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`
+    }
+  })
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress)
+  }, [progress])
+
   return (
     <View
       className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <View
-        className={clsx('h-3 rounded-xl', {
-          ['bg-zinc-800 border-zinc-700']: progress === 0,
-          ['bg-violet-900 border-violet-800']: progress > 0 && progress < 25,
+      <Animated.View
+        className=
+        {clsx('h-3 rounded-xl ', {
+          ['bg-violet-900 border-violet-800']: progress >= 0 && progress < 25,
           ['bg-violet-800 border-violet-700']: progress >= 25 && progress < 50,
           ['bg-violet-700 border-violet-600']: progress >= 50 && progress < 75,
           ['bg-violet-600 border-violet-500']: progress >= 75 && progress < 100,
           ['bg-violet-500 border-violet-400']: progress === 100
         })}
-        style={{ width: `${progress}%` }}>
-
-      </View>
+        style={style} />
     </View>
   )
 }
